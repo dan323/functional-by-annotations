db4413f63802a1f16a4af387d4013244ae8cc710
daniel <danconsa@hotmail.com>
2022-08-20 00:22:59 +0200

    open zipper

--- example-functional/src/main/java/module-info.java
+++ example-functional/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module functional.data {
     opens com.dan323.functional.data.either to functional.annotations;
     opens com.dan323.functional.data.function to functional.annotations;
     opens com.dan323.functional.data.list to functional.annotations;
+    opens com.dan323.functional.data.list.zipper to functional.annotations;
     opens com.dan323.functional.data.optional to functional.annotations;
     opens com.dan323.functional.data.tree to functional.annotations;
 }

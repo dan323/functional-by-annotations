2f202fdb0e87afe3040838a504d3ac108ef4eabf
daniel <danconsa@hotmail.com>
2022-08-28 01:31:22 +0200

    more tests

--- example-functional/src/test/java/com/dan323/functional/ZipApplicativeTest.java
+++ example-functional/src/test/java/com/dan323/functional/ZipApplicativeTest.java
@@ -23,12 +23,16 @@ public class ZipApplicativeTest {
         ZipApplicative zipApplicative = new ZipApplicative();
         List<Integer> lst = ApplicativeUtil.liftA2(zipApplicative, List.class, Integer::sum, List.repeat(5), FiniteList.of(1, 2, 3));
         assertEquals(FiniteList.of(6, 7, 8), lst);
+        lst = ApplicativeUtil.liftA2(zipApplicative, List.class, Integer::sum, FiniteList.of(1, 2, 3), List.repeat(5));
+        assertEquals(FiniteList.of(6, 7, 8), lst);
     }
 
     @Test
     public void zipGenerating() {
         ZipApplicative zipApplicative = new ZipApplicative();
         List<Integer> lst = ApplicativeUtil.liftA2(zipApplicative, List.class, Integer::sum, List.repeat(5), List.generate(1, x -> x * 2));
+        assertEquals(FiniteList.of(6, 7, 9), lst.limit(3));
+        lst = ApplicativeUtil.liftA2(zipApplicative, List.class, Integer::sum, List.generate(1, x -> x * 2), List.repeat(5));
         assertEquals(FiniteList.of(6, 7, 9), lst.limit(3));
         lst = ApplicativeUtil.liftA2(zipApplicative, List.class, Integer::sum, List.generate(1, x -> x + 1), List.generate(1, x -> x * 2));
         assertEquals(FiniteList.of(2, 4, 7), lst.limit(3));
@@ -37,7 +41,7 @@ public class ZipApplicativeTest {
     @Test
     public void zippedMap() {
         ZipApplicative zipApplicative = new ZipApplicative();
-        List<Integer> lst = FunctorUtil.map(zipApplicative, List.class, ApplicativeUtil.liftA2(zipApplicative, List.class, Integer::sum, List.generate(1, x -> x + 1), List.generate(1, x -> x * 2)), (Integer x) -> x-1);
+        List<Integer> lst = FunctorUtil.map(zipApplicative, List.class, ApplicativeUtil.liftA2(zipApplicative, List.class, Integer::sum, List.generate(1, x -> x + 1), List.generate(1, x -> x * 2)), (Integer x) -> x - 1);
         assertEquals(FiniteList.of(1, 3, 6), lst.limit(3));
     }
 }

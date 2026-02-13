b321a0ec275be47e3d20a4712f9de1e36cd1a00c
Daniel <32780500+dan323@users.noreply.github.com>
2026-02-14 00:03:27 +0100

    bump to 1.2-SNAPSHOT (#27)
    * bump to 1.2-SNAPSHOT

--- functional-definitions/functional-compiler/src/test/java/com/dan323/functional/annotation/compiler/monoid/MonoidCompilerTest.java
+++ functional-definitions/functional-compiler/src/test/java/com/dan323/functional/annotation/compiler/monoid/MonoidCompilerTest.java
@@ -17,7 +17,7 @@ public class MonoidCompilerTest {
         List<String> args = Stream.of("src/test/java/com/dan323/functional/annotation/compiler/monoid/MonoidInteger")
                 .map(s -> Paths.get(s + ".java").toAbsolutePath().toString())
                 .collect(Collectors.toList());
-        args.addAll(0, List.of("-processor", FunctionalCompiler.class.getName(), "-p", "../annotation-definitions/target/annotation-definitions-1.1-SNAPSHOT.jar", "--add-modules", "functional.annotations"));
+        args.addAll(0, List.of("-processor", FunctionalCompiler.class.getName(), "-p", "../annotation-definitions/target/annotation-definitions-1.2-SNAPSHOT.jar", "--add-modules", "functional.annotations"));
         String[] flags = args.toArray(new String[4]);
         int k = ToolProvider.getSystemJavaCompiler()
                 .run(System.in, System.out, System.err, flags);
@@ -29,7 +29,7 @@ public class MonoidCompilerTest {
         List<String> args = Stream.of("src/test/java/com/dan323/functional/annotation/compiler/monoid/NotPublicTypeMonoid")
                 .map(s -> Paths.get(s + ".java").toAbsolutePath().toString())
                 .collect(Collectors.toList());
-        args.addAll(0, List.of("-processor", FunctionalCompiler.class.getName(), "-p", "../annotation-definitions/target/annotation-definitions-1.1-SNAPSHOT.jar", "--add-modules", "functional.annotations"));
+        args.addAll(0, List.of("-processor", FunctionalCompiler.class.getName(), "-p", "../annotation-definitions/target/annotation-definitions-1.2-SNAPSHOT.jar", "--add-modules", "functional.annotations"));
         String[] flags = args.toArray(new String[4]);
         int k = ToolProvider.getSystemJavaCompiler()
                 .run(System.in, System.out, System.err, flags);

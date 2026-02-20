c96a608eb4b267bd13ace2fac5df6d1bfb6533fc
dnl232 <danconsa@hotmail.com>
2026-02-20 23:54:24 +0100

    cycled lists

--- example-functional/src/main/java/com/dan323/functional/data/list/Cycle.java
+++ example-functional/src/main/java/com/dan323/functional/data/list/Cycle.java
@@ -4,28 +4,28 @@ import java.util.function.Function;
 
 public final class Cycle<A> extends InfiniteList<A> {
 
-    private final FiniteList<A> cycled;
+    private final FiniteList<A> cycle;
 
     Cycle(FiniteList<A> cycle) {
         if (cycle == null || cycle.length() == 0) {
             throw new IllegalArgumentException("The list to be cycled must not be null or empty.");
         }
-        this.cycled = cycle;
+        this.cycle = cycle;
     }
 
     @Override
     public A getHead() {
-        return cycled.head().maybe(Function.identity(), null);
+        return cycle.head().maybe(Function.identity(), null);
     }
 
     @Override
     public InfiniteList<A> tail() {
-        var init = cycled.tail();
-        return ListUtils.concat(init, this);
+        var init = cycle.tail();
+        return (InfiniteList<A>) ListUtils.concat(init, this);
     }
 
     @Override
     public <B> Cycle<B> map(Function<A, B> mapping) {
-        return new Cycle<>(cycled.map(mapping));
+        return new Cycle<>(cycle.map(mapping));
     }
 }
